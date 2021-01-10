21e5a61aa85c37e4a2ffe199425075f250b868d5
StarsEnd <1161386515@qq.com>
2021-01-10 23:19:24 +0800

    gui v0.0.1 fix problems append auto-JScrollPane append icon echo v0.1.0 append udp-echo fix problems

--- testCilent/src/UdpClient.java
+++ testCilent/src/UdpClient.java
@@ -3,6 +3,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author StarsEnd
@@ -40,7 +41,7 @@ public class UdpClient {
     public void send(String msg){
         try {
             //先准备一个待发送的数据报
-            byte[] outputData=msg.getBytes("GB2312");
+            byte[] outputData=msg.getBytes(StandardCharsets.UTF_8);
             //构建一个数据报文。
             DatagramPacket outputPacket=new DatagramPacket(outputData,
                     outputData.length,remoteIP,remotePort);
@@ -53,13 +54,13 @@ public class UdpClient {
     public String receive(){//throws IOException{
         String msg;
         //先准备一个空数据报文
-        DatagramPacket inputPacket=new DatagramPacket(new byte[512],512);
+        DatagramPacket inputPacket=new DatagramPacket(new byte[1024],1024);
         try {
             //阻塞语句，有数据就装包，以装完或装满为此.
             socket.receive(inputPacket);
             //从报文中取出字节数据并装饰成字符。
-            msg=new String(inputPacket.getData(),
-                    0,inputPacket.getLength(),"GB2312");
+            msg="Server:"+(new String(inputPacket.getData(),
+                    0,inputPacket.getLength(), StandardCharsets.UTF_8));
         } catch (IOException ex) {
             msg=null;
         }
--- testCilent/src/clientGUI.java
+++ testCilent/src/clientGUI.java
@@ -1,6 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -34,13 +38,16 @@ public class clientGUI {
                             try{
                                 msg = udpClient.receive();
                             } catch(Exception ex){
-                                textArea1.append("套接字异常关闭"+"\n");
+                                //textArea1.append("套接字异常关闭"+"\n");
+                                appendTextArea("套接字异常关闭"+"\n");
                             }
                             if(msg!=null){
-                                textArea1.append(msg+"\n");
+                                //textArea1.append(msg+"\n");
+                                appendTextArea(msg+"\n");
                             }
                             else{
-                                textArea1.append("你的对话已关闭！"+"\n");
+                                //textArea1.append("你的对话已关闭！"+"\n");
+                                appendTextArea("你的对话已关闭！"+"\n");
                                 break;
                             }
                         }
@@ -51,7 +58,10 @@ public class clientGUI {
         sendButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                var msg =textField3.getText();
+                //textArea1.append("Client:"+msg+"\t\n");
+                appendTextArea("Client:"+msg+"\t\n");
+            udpClient.send(msg);
             }
         });
     }
@@ -61,7 +71,26 @@ public class clientGUI {
         frame.setContentPane(new clientGUI().main);
         frame.setSize(500,350);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            Image img = ImageIO.read(new FileInputStream("C:\\Users\\StarsEnd\\Documents\\GitHub\\BEACON_Server\\testCilent\\src\\1.png"));
+            frame.setIconImage(img);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         frame.setVisible(true);
+    }
+
+    //每次需要在JTextArea增加String时,调用如下方法
+    private void appendTextArea(String text){
+        textArea1.append(text);
+
+        //在更新logArea后，稍稍延时，否则getMaximum（）获得的数据可能不是最后的最大值，无法滚动到最后一行
+        try {
+            Thread.sleep(70);
+        } catch (InterruptedException ex) {
+
+        }
+        scroll.getVerticalScrollBar().setValue(scroll.getVerticalScrollBar().getMaximum());
     }
 
     private JTextArea textArea1;
@@ -73,5 +102,6 @@ public class clientGUI {
     private JButton button3;
     private JButton button4;
     private JButton button5;
+    private JScrollPane scroll;
 
 }
